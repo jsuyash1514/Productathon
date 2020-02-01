e37f2f66495004e6cf4e012a114cd1e8e2b7c6d5
singhalsara8 <singhalsara48@gmail.com>
2020-02-02 00:56:28 +0530

    implement sign up fragment

--- app/src/main/java/com/example/android/gharkikheti/FeedFragment.java
+++ app/src/main/java/com/example/android/gharkikheti/FeedFragment.java
@@ -4,13 +4,24 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedFragment extends Fragment {
+    private Button button;
+    private FirebaseAuth firebaseAuth;
+    private NavController navController;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -26,5 +37,22 @@ public class FeedFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button=view.findViewById(R.id.logout);
+        firebaseAuth=FirebaseAuth.getInstance();
+        navController= Navigation.findNavController(getActivity(),R.id.my_nav_host_fragment);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                NavOptions navOptions=new NavOptions.Builder().setPopUpTo(R.id.feedFragment,true).build();
+                navController.navigate(R.id.action_feedFragment_to_loginFragment,null,navOptions);
+            }
+        });
+
     }
 }
