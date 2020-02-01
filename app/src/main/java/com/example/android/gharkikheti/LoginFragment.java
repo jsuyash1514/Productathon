2a4b28682615b5ffe65decebdeef7bb0bf7abf67
singhalsara8 <singhalsara48@gmail.com>
2020-02-01 19:03:16 +0530

    implement login

--- app/src/main/java/com/example/android/gharkikheti/LoginFragment.java
+++ app/src/main/java/com/example/android/gharkikheti/LoginFragment.java
@@ -10,11 +10,17 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,7 +28,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class LoginFragment extends Fragment {
     private EditText memail, mpassword;
     private FirebaseAuth fAuth;
-    private DatabaseReference ref;
+    private Button mlogin;
     private NavController navController;
     private ProgressDialog progressDialog;
 
@@ -46,10 +52,39 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        memail=view.findViewById(R.id.email_id);
-        mpassword=view.findViewById(R.id.password);
-        fAuth=FirebaseAuth.getInstance();
-        ref= FirebaseDatabase.getInstance().getReference();
-        fAuth.signInWithEmailAndPassword()
+        mlogin = view.findViewById(R.id.login_btn);
+        memail = view.findViewById(R.id.email_id);
+        mpassword = view.findViewById(R.id.password);
+        fAuth = FirebaseAuth.getInstance();
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = memail.getText().toString().trim();
+                String password = mpassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    memail.setError("Email is Required");
+                    return;
+                } else if (TextUtils.isEmpty(password)) {
+                    mpassword.setError("Password is Required");
+                    return;
+                }
+                progressDialog = new ProgressDialog(getContext());
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.setMessage("Logging in please wait....");
+                progressDialog.show();
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getContext(), "login successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(getContext(), "Login unsuccessful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
