4fa01f7ba491b7e13e89209bba12903f87c09935
guptaprakhariitr <prakharpguptag@gmail.com>
2020-02-02 06:56:41 +0530

    JUST RETRIEVAL LEFT

---
+++ app/src/main/java/com/example/android/gharkikheti/ProfileFragment.java
@@ -0,0 +1,205 @@
+package com.example.android.gharkikheti;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+private ArrayList<String> mcropnames=new ArrayList<>();
+    private ArrayList<String> mstartdate=new ArrayList<>();
+    private ArrayList<String> menddate=new ArrayList<>();
+    private ArrayList<String> mImageurls=new ArrayList<>();
+    EditText mcrop_name_text;
+    EditText mstart_date_text;
+    EditText mend_date_text;
+    ProgressBar mProgressBar;
+    Button mupload,mchoose;
+    ImageView mImageView;
+    private Uri mImageUri;
+    private DatabaseReference mDatabaseRef;
+    private StorageReference mStorageRef;
+    private StorageTask mUploadTask;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        mcrop_name_text=view.findViewById(R.id.Crop_name_text);
+        mstart_date_text=view.findViewById(R.id.start_date_text);
+        mend_date_text=view.findViewById(R.id.end_date_text);
+        mupload=view.findViewById(R.id.upload_crop_details);
+        mchoose=view.findViewById(R.id.choose_crop_details);
+        mImageView=view.findViewById(R.id.image_upload_display);
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+        mchoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,PICK_IMAGE_REQUEST);
+            }
+        });
+        mupload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile();
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(mImageView);
+        }
+    }
+    // TODO: Rename method, update argument and hook method into UI event
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+    private void uploadFile() {
+        if (mImageUri != null) {
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                }
+                            }, 500);
+
+                            upload_need.Upload upload = new upload_need.Upload(mcrop_name_text.getText().toString().trim(),
+                                    mstart_date_text.getText().toString().trim(),mend_date_text.getText().toString().trim(),
+                                    taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+                            String uploadId = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        }
+                    });
+            mcrop_name_text.setText(null);
+            mstart_date_text.setText(null);
+            mend_date_text.setText(null);
+            mImageView.setImageBitmap(null);
+        } else {
+
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+
+}
--- app/src/main/java/com/example/android/gharkikheti/ProfileFragmentRecyclerAdapter.java
+++ app/src/main/java/com/example/android/gharkikheti/ProfileFragmentRecyclerAdapter.java
@@ -11,16 +11,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
-public class crop_recycler_view_class extends  RecyclerView.Adapter<crop_recycler_view_class.ViewHolder>{
-   private ArrayList<String> mcrop_image=new ArrayList<>();
+public class ProfileFragmentRecyclerAdapter extends  RecyclerView.Adapter<ProfileFragmentRecyclerAdapter.ViewHolder>{
+    private ArrayList<String> mcrop_image=new ArrayList<>();
     private ArrayList<String> mcrop_name=new ArrayList<>();
     private ArrayList<String> mstart_date=new ArrayList<>();
     private ArrayList<String> mend_date=new ArrayList<>();
     private Context context;
 
-    public crop_recycler_view_class(Context context,ArrayList<String> mcrop_image, ArrayList<String> mcrop_name, ArrayList<String> mstart_date, ArrayList<String> mend_date) {
+    public ProfileFragmentRecyclerAdapter(Context context,ArrayList<String> mcrop_image, ArrayList<String> mcrop_name, ArrayList<String> mstart_date, ArrayList<String> mend_date) {
         this.mcrop_image = mcrop_image;
         this.mcrop_name = mcrop_name;
         this.mstart_date = mstart_date;
@@ -30,20 +32,26 @@ public class crop_recycler_view_class extends  RecyclerView.Adapter<crop_recycle
 
     @NonNull
     @Override
-    public crop_recycler_view_class.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_crop_recycler_items,parent,false);
-    ViewHolder holder=new ViewHolder(view);
+    public ProfileFragmentRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_crop_recycler_items,parent,false);
+        ViewHolder holder=new ViewHolder(view);
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull crop_recycler_view_class.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ProfileFragmentRecyclerAdapter.ViewHolder holder, int position) {
+        Glide.with(context)
+                .asBitmap()
+                .load(mcrop_image.get(position))
+                .into(holder.crop_image);
+        holder.Crop_name.setText(mcrop_name.get(position));
+        holder.end_date.setText(mend_date.get(position));
+        holder.start_date.setText(mstart_date.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mcrop_image.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
